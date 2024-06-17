3d1a519cd48e5ac8cd54f6e459b36e4fb34bc6dc
OswaldoLeon <oswaldoleon72@gmail.com>
2024-06-17 08:21:18 -0400

    cambios para hacer el pull

--- curso-python-pip-master/app/main.py
+++ curso-python-pip-master/app/main.py
@@ -28,6 +28,7 @@ def run():
     print(country)
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country'], labels, values)
+    charts.generate_pie_chart(country['Country'], labels, values)
 
 if __name__ == '__main__':
   run()
