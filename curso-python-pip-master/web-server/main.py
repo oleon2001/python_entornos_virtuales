292f013d8544a7d21d4037770920141a6a1e63a9
oleon2001 <oswaldoleon72@gmail.com>
2024-06-20 11:30:02 -0400

    comentarios en los archivos docker y del archivo store y app

--- curso-python-pip-master/web-server/main.py
+++ curso-python-pip-master/web-server/main.py
@@ -29,7 +29,7 @@ def get_list():
     # Devolvemos una cadena HTML que contiene un título y un párrafo
     return """
         <h1>HOLA ADRIAN :)</h1>
-        <p>TE ODIO CON TODAS LAS FUERZAS QUE UN HOMBRE PUEDE ODIAR PEGATE</p>
+        <p>TE ODIO CON TODAS LAS FUERZAS QUE UN HOMBRE PUEDE ODIAR- PEGATE</p>
     """
 
 # Definimos una función 'run' que se ejecutará cuando se ejecute el script
@@ -71,4 +71,64 @@ pip3 freeze
     Actualizar Requirements
 
  pip3 freeze > requirements.txt
+
+ correr el servidor de uvicorn
+
+ uvicorn main:adrianTeOdio --reload
+
+ main es el archivo donde se esta ejecutando
+ adrianTeOdio es el objeto que contiene al fastApi para hacer las peticiones
+ --reload es para que se reinicie el servidor cada vez que se hace un cambio en el código
+ 
+
+'''
+
+
+'''
+añadidos adicionales
+
+
+
+    FastAPI
+
+    Es un framework de Python para crear aplicaciones web
+      rápidas y seguras. Utilice la mejor OpenAPI para definir la 
+      interfaz de la aplicación y proporcione un conjunto de herramientas
+        para validar y documentar la API de manera automática.
+
+    ****Uvicorn ****
+
+    Es un servidor ASGI (Asynchronous Server Gateway Interface) de alto rendimiento 
+    para ejecutar aplicaciones ASGI como FastAPI. Es una alternativa a otros servidores ASGI 
+    como Daphne y Hypercorn.
+
+    FastAPI y Uvicorn se utilizan 
+    juntos para proporcionar un entorno rápido y fácil de usar 
+    para el desarrollo y el uso de aplicaciones web basadas en ASGI.
+
+'''
+
+
+'''
+un endpoint es una URL específica que una aplicación utiliza para interactuar con el 
+cliente (usualmente un navegador web o una aplicación móvil) para realizar una acción específica o recuperar datos específicos.
+
+Piensa en un endpoint como una puerta de entrada a un recurso o funcionalidad específica dentro de una a
+plicación. Cuando un cliente (como un navegador web) envía una solicitud a un endpoint, la aplicación procesa
+la solicitud y devuelve una respuesta, que puede ser en forma de datos, HTML, JSON o incluso un mensaje de error.
+
+En el código proporcionado, tenemos dos endpoints:
+
+@adrianTeOdio.get('/') - Este endpoint se define en la URL raíz (/) y devuelve una lista de enteros [1, 2, 3].
+@adrianTeOdio.get('/contact', response_class=HTMLResponse) - Este endpoint se define en 
+la URL /contact y devuelve una respuesta HTML con un título y un párrafo.
+Los endpoints se pueden definir utilizando diferentes métodos HTTP, como:
+
+GET: Recupera datos del servidor.
+POST: Envía datos al servidor para crear un nuevo recurso.
+PUT: Actualiza un recurso existente en el servidor.
+DELETE: Elimina un recurso en el servidor.
+En FastAPI, los endpoints se definen utilizando decoradores, 
+como @app.get('/') o @app.post('/create'), que especifican el método HTTP y la ruta URL para el endpoint.
+
 '''
